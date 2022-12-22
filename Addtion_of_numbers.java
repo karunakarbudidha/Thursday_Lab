ea407d87534186e43e0a341dcd079376a951378d
karunakarbudidha <121231515+karunakarbudidha@users.noreply.github.com>
2022-12-22 16:43:00 +0530

    Add files via upload

---
+++ Addtion_of_numbers.java
@@ -0,0 +1,11 @@
+package javaProject;
+
+public class Addtion_of_numbers {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=30;
+		System.out.println("printing the squre of two numbers "+(a+b)*2);
+	}
+
+}
---
+++ Area_of_squre.java
@@ -0,0 +1,14 @@
+package javaProject;
+
+public class Area_of_squre {
+
+	public static void main(String[] args) {
+		
+		int hight =10;
+		int whight =20;
+		int area = hight*whight;
+		System.out.println("Printing Area of squres ="+area);   
+
+	}
+
+}
