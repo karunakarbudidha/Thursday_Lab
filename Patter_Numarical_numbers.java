05218d33e8726efdc86f8b2e7357e906ae891358
karunakarbudidha <121231515+karunakarbudidha@users.noreply.github.com>
2022-12-30 11:23:16 +0530

    Add files via upload

---
+++ Patter_Numarical_numbers.java
@@ -0,0 +1,19 @@
+package javaProject;
+
+public class Patter_Numarical_numbers {
+
+	public static void main(String[] args) {
+	int i,j,count=0;
+		for(i=1; i<=4;i++) {
+			for(j=1;j<=i;j++) {
+				count=count+1;
+				System.out.print(count);
+			}
+		
+			System.out.println();
+			}
+		
+		}
+ }
+
+
